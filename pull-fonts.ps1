aa3fff6b85db7685b3055ac734888554f945b77a
wormeyman <wormeyman@gmail.com>
2014-01-15 10:52:48 -0800

    Added my resource

--- pull-fonts.ps1
+++ pull-fonts.ps1
@@ -1,4 +1,5 @@
-﻿#(New-Object System.Drawing.Text.InstalledFontCollection).Families
+﻿#http://technet.microsoft.com/en-us/library/ff730944.aspx
+#(New-Object System.Drawing.Text.InstalledFontCollection).Families
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
 
     $objFonts = New-Object System.Drawing.Text.InstalledFontCollection
