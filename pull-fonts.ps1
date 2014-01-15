171fef799b8b20095026a6508ad8e4d44026d001
wormeyman <wormeyman@gmail.com>
2014-01-15 10:51:20 -0800

    Now works when i script remotely.

--- pull-fonts.ps1
+++ pull-fonts.ps1
@@ -1,1 +1,5 @@
-﻿(New-Object System.Drawing.Text.InstalledFontCollection).Families
+﻿#(New-Object System.Drawing.Text.InstalledFontCollection).Families
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+
+    $objFonts = New-Object System.Drawing.Text.InstalledFontCollection
+    $objFonts.Families
